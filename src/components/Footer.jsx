4e847beb0f264ae98ea840844e35301a039c5d82
Omarespo-dev <omaresposito21@gmail.com>
2025-03-19 00:09:47 +0100

    ADDED INPUT SEARCH .LENTGH AND FOOTER

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
     <footer>
       <p> &copy; 2025 - Tutti i diritti riservati</p>
       <nav >
-        <a href="#faq">FAQ</a>
-        <a href="#contatti">Contatti</a>
-        <a href="#privacy">Privacy Policy</a>
+        <a href="#">FAQ</a>
+        <a href="#">Contatti</a>
+        <a href="#">Privacy Policy</a>
       </nav>
     </footer>
   );
